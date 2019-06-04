2243e75ceb7bd93f11d129b7dd8840e4bb5aa076
koanz <cristiananzawa@gmail.com>
2019-06-04 08:32:48 -0300

    se agrega función de slider

--- public/js/materialize-functions.js
+++ public/js/materialize-functions.js
@@ -1,27 +1,9 @@
-$(window).on("load", function(){
-    checkTopPosition();
-
-    function checkTopPosition(){
-        var coverElement = $(".cover").height();
-        if(document.documentElement.scrollTop >= coverElement){
-            $(".nav-brand-logo").addClass("logo-display");
-            $(".nav-brand-logo").removeClass("logo-hide");
-        }
-    }
-});
-
 $(document).ready(function(){
-    
-
     $('.sidenav').sidenav();
 
     $('.scrollspy').scrollSpy();
 
-    //$('.parallax').parallax();
-
-    $('.slider').slider({
-        indicators: false
-    });
+    $('.slider').slider();
 
     $("#top-button").click(function() {
         $("html, body").animate({ scrollTop: 0 }, "slow");
@@ -29,9 +11,6 @@ $(document).ready(function(){
     });
 
     $('.materialboxed').materialbox();
-
-    //$('.tooltipped').tooltip();
-
 
     $(".sidenav").click(function(){
         var instance = M.Sidenav.getInstance(document.getElementById("mobile"));
